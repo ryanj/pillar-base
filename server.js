3b3e8ba67e2e7760f5b6a225d93faa75be399a2f
ryanj <ryan.jarvinen@gmail.com>
2016-01-31 07:45:11 -0800

    a basic pillarjs example

---
+++ server.js
@@ -0,0 +1,36 @@
+var cc       = require('config-multipaas'),
+    finalhandler= require('finalhandler'),
+    http     = require("http"),
+    Router       = require('router'),
+    fs = require('fs'),
+    serveStatic       = require("serve-static");
+
+var config   = cc();
+var app      = Router()
+var index    = fs.readFileSync(__dirname + '/index.html');
+
+// Serve up public/ftp folder 
+app.use(serveStatic('static'))
+
+// Routes
+app.get("/status", function (req, res) {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'application/json; charset=utf-8')
+  res.end("{status: 'ok'}\n")
+})
+
+app.get("/", function (req, res) {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/html; charset=utf-8')
+  res.end(index.toString())
+})
+
+// Create server 
+var server = http.createServer(function(req, res){
+  var done = finalhandler(req, res)
+  app(req, res, done)
+})
+
+server.listen(config.get('PORT'), config.get('IP'), function () {
+  console.log( "Listening on " + config.get('IP') + ", port " + config.get('PORT') )
+});
